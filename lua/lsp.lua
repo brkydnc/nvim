29ee7cf302259e52587305c83bba7b4379a5cf78
Berkay Dinç <berkayydinc@gmail.com>
2023-08-01 12:46:08 +0300

    add clangd

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -50,7 +50,7 @@ settings = {
   }
 }
 
-local servers = { "rust_analyzer" }
+local servers = { "rust_analyzer", "clangd" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup { on_attach = on_attach, settings = settings[server] }
 end
